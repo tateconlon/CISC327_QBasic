cf2618f43548be9ec563508a68fc50831f7ba712
Tate Conlon <tate.conlon@gmail.com>
2017-10-14 21:27:04 -0400

    Added a Test class that holds a test's data

--- src/main.py
+++ src/main.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python3
+import csv
+
+
+
+def readTestInputCsv(transactionName):
+	'''
+	Reads the inputs for a transactions test suite and returns
+	a list of tests, each which contains a list of input for that test
+	'''
+	filename = "../tests/{0}/inputs.csv".format(transactionName)
+	testInputList = []
+
+	with open(filename, "r") as f:
+		csv_data = csv.reader(f, delimiter = ",")
+
+		for row in csv_data:
+			testInputList.append(row)
+
+	return testInputList
 
 def main():
-	print("Hello World")
+	print(readTestInputCsv("createacct"))
 
 if __name__ == "__main__":
 	main()
