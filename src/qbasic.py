d282d4e65f54144e8b8d7e1e392d1d1362c3b14d
Tate Conlon <tate.conlon@gmail.com>
2017-10-16 16:34:56 -0400

    more skelly

--- src/qbasic.py
+++ src/qbasic.py
@@ -1,15 +1,21 @@
-
-
+import sys
 class QBasic():
 
 	def __init__(self):
-		self.transactionFile = []
+		self.transactionFile = [] #list of strings
+		self.validAccounts = []	#list of strings
 
-	def run(self):
+	def run(self, validAccountsFileName, transactionFileName):
+
+		pass
 
 
-	def login(self):
+
+
+
+	def login(self, validAccountsFileName):
 		'''returns permissionType as string. returns empty string if invalid login attempt.'''
+		#read valid Accounts file
 		return ""
 
 	def create_acct(self, permissionType):
@@ -25,9 +31,12 @@ class QBasic():
 		pass
 
 	def logout(self):
+		#write and clear transaction file
 		pass
 
-
+	def loadValidAccounts(self, validAccountsFileName):
+		#end at 0000000
+		pass
 
 def main():
 	q = QBasic()
