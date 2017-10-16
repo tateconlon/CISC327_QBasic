de4b09544854837f6e1f379337506de25466c8a5
Tate Conlon <tate.conlon@gmail.com>
2017-10-16 16:58:39 -0400

    For Jeffy jeff

--- src/qbasic.py
+++ src/qbasic.py
@@ -9,17 +9,29 @@ class QBasic():
 
 		pass
 
-
-
-
-
 	def login(self, validAccountsFileName):
 		'''returns permissionType as string. returns empty string if invalid login attempt.'''
 		#read valid Accounts file
-		return ""
+		permissionType = input('Please enter desired permission type (agent/machine): ')
+		if(permissionType not in ['agent', 'machine']):
+			print("login permission {0} invalid. Please choose agent or machine.".format(permissionType))
+			return ""
+		
+		loadValidAccounts(validAccountsFileName)
+		return permissionType
 
 	def create_acct(self, permissionType):
-		pass
+		if(permissionType != 'agent'):
+			print("createacct not available with permission type {0}".format(permissionType))
+		
+		accountNumber = input('Please enter the account number (8 digits): ')
+		if accountNumber in validAccounts:
+			print("cannot create account number {0} as it already exists".format(accountNumber))
+			return
+
+		#validate account
+
+		transLine = "NEW {0} 000 00000000 {1}"
 
 	def delete_acct(self, permissionType):
 		pass
