396cde04abd4ff320d8b8605abc78b1cdd0b7f9c
Tate Conlon <tate.conlon@gmail.com>
2017-10-16 16:16:36 -0400

    hey

---
+++ src/qbasic.py
@@ -0,0 +1,33 @@
+
+
+class QBasic():
+
+
+
+
+
+
+
+	def login(self):
+		'''returns permissionType as string. returns empty string if invalid login attempt.'''
+		return ""
+
+	def create_acct(self, permissionType):
+		pass
+
+	def delete_acct(self, permissionType):
+		pass
+
+	def transfer(self, permissionType):
+		pass
+
+	def 
+
+
+
+def main():
+	q = QBasic()
+	q.run()
+
+if __name__ == "__main__":
+	main()
