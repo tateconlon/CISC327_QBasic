bbf96608ab563c301b1fd3f460ecfcf738333c9b
Tate Conlon <tate.conlon@gmail.com>
2017-10-16 21:07:58 -0400

    full login/logout flow except for deleteacct completed

--- src/qbasicTate.py
+++ src/qbasicTate.py
@@ -6,10 +6,20 @@ class QBasic():
 		self.validAccounts = []	#list of strings
 
 	def run(self, validAccountsFileName, transactionFileName):
+		print('Welcome to QBasic!')
+		while True:
+			#log in
+			while(input('Please type login to log in: ') != "login"):
+				pass
+			permissionType = self.login(validAccountsFileName)
+			
+			if(permissionType == ""):	#log in unsuccessful
+				continue #go back to top of while True
+			
+			#log in successful
+			self.loggedInState(permissionType, transactionFileName)
 
-		pass
-
-	def loggedInState(self, permissionType):
+	def loggedInState(self, permissionType, transactionFileName):
 		loggedOut = False
 		while (not loggedOut):
 			transactionInput = input("Input the transaction code: ")
@@ -21,13 +31,14 @@ class QBasic():
 				self.transfer(permissionType)
 			elif transactionInput == "logout":
 				loggedOut = True
-				self.logout()
+				self.logout(transactionFileName)
 			elif transactionInput == "createacct":
 				self.create_acct(permissionType)
 			elif transactionInput == "deleteacct":
 				self.delete_acct(permissionType)
 			else:
-				print("transaction code {0} invalid.".format(transactionInput))	
+				print("transaction code {0} invalid.".format(transactionInput))
+
 
 	def login(self, validAccountsFileName):
 		'''returns permissionType as string. returns empty string if invalid login attempt.'''
@@ -77,6 +88,10 @@ class QBasic():
 		3) Dep Amt less than $1000.00 for machine, less than $999,999.99 for agent
 		4) Writes to transaction file
 		'''
+		if permissionType not in ['agent', 'machine']:
+			print('Cannot access deposit with permission type {0}'.format(permissionType))
+			return 
+
 		accountNumber = input('Please enter the account number to deposit into: ')
 		if(accountNumber not in self.validAccounts):
 			print('Account {0} does not exist'.format(accountNumber))
@@ -89,11 +104,11 @@ class QBasic():
 			print('{0} is not a valid number'.format(depAmtStr))
 			return
 
-		if(permissionType == 'agent' and deptAmt > 99999999):
+		if(permissionType == 'agent' and depAmt > 99999999):
 			print('Cannot deposit more than $999,999.99 in a single transaction in agent mode')
 			return
 
-		if(permissionType == 'machine' and deptAmt > 100000):
+		if(permissionType == 'machine' and depAmt > 100000):
 			print('Cannot deposit more than $1000.00 in a single transaction in machine mode')
 			return
 
@@ -105,18 +120,64 @@ class QBasic():
 		
 
 	def transfer(self, permissionType):
+		'''Transfer money from an account to another. Both accounts must be created by the backend.
+		1) Asks for valid account number FROM.
+		2) Asks for valid account number TO.
+		3) Asks for amount to transfer in cents.
+		3) Transfer Amt less than $1000.00 for machine, less than $999,999.99 for agent
+		4) Writes to transaction file
+		'''
+		if permissionType not in ['agent', 'machine']:
+			print('Cannot access transfer with permission type {0}'.format(permissionType))
+			return 
+
+		accountNumber1 = input('Please enter the account number to tranfer from: ')
+		if(accountNumber1 not in self.validAccounts):
+			print('Account {0} does not exist'.format(accountNumber1))
+			return
+
+		accountNumber2 = input('Please enter the account number to tranfer from: ')
+		if(accountNumber2 not in self.validAccounts):
+			print('Account {0} does not exist'.format(accountNumber2))
+			return
+		if(accountNumber2 == accountNumber1):
+			print('{0} -> {1}. Cannot transfer into the same account'.format(accountNumber1, accountNumber2))
+			return
+
+		transAmtStr = input('Please enter the amount to deposit (in cents): ')
+		try:
+			transAmt = int(transAmtStr)
+		except ValueError:
+			print('{0} is not a valid number'.format(transAmtStr))
+			return
+
+		if(permissionType == 'agent' and transAmt > 99999999):
+			print('Cannot deposit more than $999,999.99 in a single transaction in agent mode')
+			return
+
+		if(permissionType == 'machine' and transAmt > 100000):
+			print('Cannot deposit more than $1000.00 in a single transaction in machine mode')
+			return
+
+		newTransLine = "XFR {0} {1} {2} ***".format(accountNumber1, transAmtStr, accountNumber2)
+		self.transactionFile.append(newTransLine)
+
+		print('transfer of {1} cents from {0} to {2} successful.'.format(accountNumber, transAmtStr, accountNumber2))
 		pass
 
-	def logout(self):
-		#write and clear transaction file
-		pass
+	def logout(self, transactionSummaryFile):
+		'''Writes the transaction summary file'''
+		self.transactionFile.append("EOS")
+		with open(transactionSummaryFile, "w") as f:
+			f.write('\n'.join(self.transactionFile))
+		
+		self.transactionFile = [] #clear transactionFile
 
 	def loadValidAccounts(self, validAccountsFileName):
 		'''loads self.validAccounts from valid accounts file. Assumes Valid Accounts file is formatted perfectly'''
 		with open(validAccountsFileName, "r") as f:
 			lines = [x.strip() for x in f.readlines()]
 		self.validAccounts = lines[:-1] #0000000 is the last line
-		pass
 
 	def isNameValid(self, nameStr):
 		'''Checks if name is between 3-30 characters, [A-Z][a-z][0-9] without leading/trailing spaces'''
@@ -133,9 +194,7 @@ class QBasic():
 
 def main():
 	q = QBasic()
-	q.login("validAccounts.txt")
-	print(q.validAccounts)
-	q.loggedInState('agent')
+	q.run("validAccounts.txt", "transactionSummary.txt")
 
 if __name__ == "__main__":
 	main()
