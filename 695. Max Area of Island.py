82bd03016b2b2b15b132f13f87178d76b471049e
Calum Bruton <calumbruton@gmail.com>
2017-11-18 23:26:08 -0500

    finish this

---
+++ 695. Max Area of Island.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def maxAreaOfIsland(self, grid):
+        m, n = len(grid), len(grid[0])
+        
+        islands = []
+        
+        def dfs(i,j):         
+            # Fill this in
+            
+            
+            
+        for i in grid:
+            # Perform the DFS
+            
+        
+        return max(islands)
